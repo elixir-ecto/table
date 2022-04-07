7904d015056ad80f89a7d53bc6c2ece031515962
Jonatan Kłosko <jonatanklosko@gmail.com>
2022-04-07 13:16:44 +0200

    Implement table reader protocol and unified access (#1)
    * Implement table reader protocol and unified access
    
    * Do not treat list of structs as tabular
    
    * Add to_rows_with_info/2 nd to_columns_with_info/2
    
    * Change wording
    
    * Apply suggestions from code review
    
    Co-authored-by: José Valim <jose.valim@gmail.com>
    
    * Apply review comments
    
    Co-authored-by: José Valim <jose.valim@gmail.com>

---
+++ lib/table/reader.ex
@@ -0,0 +1,62 @@
+defprotocol Table.Reader do
+  @moduledoc """
+  A protocol to read tabular data.
+  """
+
+  @typedoc """
+  Describes row-based traversal.
+
+  The enumerable should yield rows, where each row is a series of
+  values. The values should follow the order of columns in the
+  metadata.
+  """
+  @type row_reader :: {:rows, metadata(), Enumerable.t(row_values :: Enumerable.t())}
+
+  @typedoc """
+  Describes column-based traversal.
+
+  The enumerable should yield columns, where each column is a series
+  of values. The columns should have the same order as columns in the
+  metadata.
+  """
+  @type column_reader :: {:columns, metadata(), Enumerable.t(column_values :: Enumerable.t())}
+
+  @typedoc """
+  Table metadata.
+  """
+  @type metadata :: %{
+          columns: list(Table.column())
+        }
+
+  @typedoc """
+  Returns information on how to traverse the given tabular data.
+
+  There are generally two distinct ways of traversing tabular data,
+  that is, a row-based one and a column-based one. Depending on the
+  underlying data representation, one of them is more natural and
+  more efficient.
+
+  This return value describes either of the two traversal types, see
+  `t:row_reader/0` and `t:column_reader/0` respectively.
+
+  Some structs may be tabular only in a subset of cases, therefore
+  `:none` may be returned to indicate that there is no valid data to
+  read.
+  """
+  @spec init(t()) :: row_reader() | column_reader() | :none
+  def init(tabular)
+end
+
+defimpl Table.Reader, for: List do
+  def init(list) do
+    with :none <- Table.Reader.Enumerable.init_columns(list),
+         :none <- Table.Reader.Enumerable.init_rows(list),
+         do: :none
+  end
+end
+
+defimpl Table.Reader, for: Map do
+  def init(map) do
+    Table.Reader.Enumerable.init_columns(map)
+  end
+end
---
+++ test/table/reader_test.exs
@@ -0,0 +1,129 @@
+defmodule Table.ReaderTest do
+  use ExUnit.Case, async: true
+
+  describe "list init/1" do
+    test "returns :none for non-tabular lists" do
+      assert Table.Reader.init([]) == :none
+      assert Table.Reader.init([1, 2, 3]) == :none
+
+      assert Table.Reader.init([[]]) == :none
+      assert Table.Reader.init([["a"]]) == :none
+
+      assert Table.Reader.init([{"a", [1, 2], [1, 2]}]) == :none
+      assert Table.Reader.init([{"a", [1, 2]}, {"b", 2}]) == :none
+
+      assert Table.Reader.init([%URI{}]) == :none
+    end
+
+    test "list of key-vals" do
+      data = [
+        [{"id", 1}, {"name", "Sherlock"}],
+        [{"id", 2}, {"name", "John"}]
+      ]
+
+      assert {:rows, %{columns: ["id", "name"]}, enum} = Table.Reader.init(data)
+      assert Enum.to_list(enum) == [[1, "Sherlock"], [2, "John"]]
+    end
+
+    test "list of maps" do
+      data = [
+        %{"id" => 1, "name" => "Sherlock"},
+        %{"id" => 2, "name" => "John"}
+      ]
+
+      assert {:rows, %{columns: ["id", "name"]}, enum} = Table.Reader.init(data)
+      assert Enum.to_list(enum) == [[1, "Sherlock"], [2, "John"]]
+    end
+
+    test "list with list series" do
+      data = [
+        {"id", [1, 2]},
+        {"name", ["Sherlock", "John"]}
+      ]
+
+      assert {:columns, %{columns: ["id", "name"]}, enum} = Table.Reader.init(data)
+      assert [ids, names] = Enum.to_list(enum)
+      assert Enum.to_list(ids) == [1, 2]
+      assert Enum.to_list(names) == ["Sherlock", "John"]
+    end
+
+    test "list with enumerable series" do
+      data = [
+        {"id", 1..2},
+        {"name", Stream.map(["Sherlock", "John"], & &1)}
+      ]
+
+      assert {:columns, %{columns: ["id", "name"]}, enum} = Table.Reader.init(data)
+      assert [ids, names] = Enum.to_list(enum)
+      assert Enum.to_list(ids) == [1, 2]
+      assert Enum.to_list(names) == ["Sherlock", "John"]
+    end
+
+    test "enumerating rows raises on invalid element" do
+      assert {:rows, %{}, enum} = Table.Reader.init([%{a: 1}, 1])
+
+      assert_raise RuntimeError, "invalid table record: 1", fn ->
+        Enum.to_list(enum)
+      end
+    end
+
+    test "enumerating rows raises on missing map element" do
+      assert {:rows, %{}, enum} =
+               Table.Reader.init([
+                 %{"x" => 1, "y" => 1},
+                 %{"x" => 2}
+               ])
+
+      assert_raise RuntimeError,
+                   ~s/map records must have the same columns, missing column "y" in %{"x" => 2}/,
+                   fn ->
+                     Enum.to_list(enum)
+                   end
+    end
+
+    test "enumerating rows raises on keyval order mismatch" do
+      assert {:rows, %{}, enum} =
+               Table.Reader.init([
+                 [{"x", 1}, {"y", 1}],
+                 [{"y", 2}, {"x", 2}]
+               ])
+
+      assert_raise RuntimeError,
+                   ~s/key-value records must have columns in the same order, expected "x", but got "y"/,
+                   fn ->
+                     Enum.to_list(enum)
+                   end
+    end
+
+    test "enumerating rows raises on invalid keyval element" do
+      assert {:rows, %{}, enum} =
+               Table.Reader.init([
+                 [{"x", 1}, {"y", 1}],
+                 [{"x", 2}, 2]
+               ])
+
+      assert_raise RuntimeError, ~s/expected a key-value pair, but got: 2/, fn ->
+        Enum.to_list(enum)
+      end
+    end
+  end
+
+  describe "map init/1" do
+    test "returns :none for non-tabular maps" do
+      assert Table.Reader.init(%{}) == :none
+      assert Table.Reader.init(%{a: [1, 2], b: 2}) == :none
+    end
+
+    test "map with series" do
+      data = %{
+        "id" => [1, 2],
+        "name" => ["Sherlock", "John"]
+      }
+
+      assert {:columns, %{columns: ["id", "name"]}, enum} = Table.Reader.init(data)
+      assert [ids, names] = Enum.to_list(enum)
+      assert Enum.to_list(ids) == [1, 2]
+      assert Enum.to_list(names) == ["Sherlock", "John"]
+    end
+  end
+end
