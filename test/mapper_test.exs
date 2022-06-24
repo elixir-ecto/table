50002ca2f910c36924f2eab9f137f1e547f9f5f2
Jonatan Kłosko <jonatanklosko@gmail.com>
2022-06-25 01:03:42 +0200

    Retain enumeration properties when using to_rows on row data

---
+++ test/mapper_test.exs
@@ -0,0 +1,18 @@
+defmodule Table.MapperTest do
+  use ExUnit.Case, async: true
+
+  alias Table.Mapper
+
+  test "reduce" do
+    enumerable = 1..3 |> Mapper.map(fn x -> x * x end)
+    assert Enum.reduce(enumerable, &(&1 + &2)) == 14
+  end
+
+  test "slice" do
+    enumerable = 1..10 |> Mapper.map(fn x -> x * x end)
+    assert Enum.slice(enumerable, 4..6) == [25, 36, 49]
+
+    enumerable = 1..10 |> Stream.map(& &1) |> Mapper.map(fn x -> x * x end)
+    assert Enum.slice(enumerable, 4..6) == [25, 36, 49]
+  end
+end
